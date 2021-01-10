72f0c57fdd0d0e83280651d2aee2c2f161f2801c
dansbrainy <47524893+dansbrainy@users.noreply.github.com>
2021-01-10 22:01:28 +0200

    palm background collapsible content

--- app/static/script/phone.js
+++ app/static/script/phone.js
@@ -38,15 +38,41 @@
 // // store the instance variable so we can access it in the console e.g. window.iti.getNumber()
 // window.iti = iti;
 
-var input = document.getElementById("tel");
-var iti = window.intlTelInput(input, {
-    utilsScript: "https://cdn.jsdelivr.net/npm/intl-tel-input@17.0.3/build/js/utils.js",
-    allowDropdown:true,
-    autoPlaceholder:"polite",
-    formatOnDisplay:true,
-    geoIpLookup:null,
-    preferredCountries: ["za","us", "gb" ],
-    utilsScript:"/app/build/js/utils.js"
+// var input = document.getElementById("tel");
+// var iti = window.intlTelInput(input, {
+//     utilsScript: "https://cdn.jsdelivr.net/npm/intl-tel-input@17.0.3/build/js/utils.js",
+//     allowDropdown:true,
+//     autoPlaceholder:"polite",
+//     formatOnDisplay:true,
+//     geoIpLookup:null,
+//     preferredCountries: ["za","us", "gb" ],
+//     utilsScript:"/app/build/js/utils.js"
+// });
+
+// window.iti = iti;
+
+
+
+const svg = new Walkway({
+    selector: '#svg-castle',
+    duration: 3000,
 });
 
-window.iti = iti;
+  
+svg.draw(after);
+  
+function after(){
+    // fillPath('color-1','#695a69');
+    // fillPath('color-2','#b2441d');
+    // fillPath('color-3','#dfd0c6');
+    // fillPath('color-4','#c8b2a8');
+    // fillPath('color-5','#de582a');
+    // fillPath('color-6','#a08a8a')
+}
+
+function fillPath(classname, color){
+  const paths = document.querySelectorAll(`#svg-castle .${classname}`);
+  for(path of paths){
+    path.style.fill = `${color}`;
+  }
+ }
